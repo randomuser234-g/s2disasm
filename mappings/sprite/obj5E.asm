6f6599a1f86180070b875108dc35030fb92cd099
randomuser234-g <thoratkihome@gmail.com>
2026-08-20 21:27:23 -0500

    add knux mappings
    i couldn't get maps for knux hud so file had to be edited

--- mappings/sprite/obj5E.asm
+++ mappings/sprite/obj5E.asm
@@ -1,33 +1,41 @@
-Map_obj5E:	mappingsTable
+Map_9815: mappingsTable
 	mappingsTableEntry.w	Map_obj5E_0008
+	mappingsTableEntry.w	Map_b02d_1
 	mappingsTableEntry.w	Map_obj5E_002A
 	mappingsTableEntry.w	Map_obj5E_004C
-	mappingsTableEntry.w	Map_obj5E_006E
+	mappingsTableEntry.w	Map_b02d_4
 
 Map_obj5E_0008:	spriteHeader
-	spritePiece	-$60, $10, 4, 1, 0, 0, 0, 1, 1
-	spritePiece	-$40, $10, 1, 1, 4, 0, 0, 1, 1
-	spritePiece	-$68, $18, 4, 2, $A, 0, 0, 2, 1
-	spritePiece	-$48, $18, 1, 2, $3C, 0, 0, 2, 1
+ spritePiece -$60, $10, 4, 1, 0, 0, 0, 1, 1
+ spritePiece -$40, $10, 1, 1, 4, 0, 0, 1, 1
+ spritePiece -$68, $18, 4, 2, $A, 0, 0, 2, 1
+ spritePiece -$48, $18, 1, 2, $3C, 0, 0, 2, 1
 Map_obj5E_0008_End
 
+Map_b02d_1:	spriteHeader
+ spritePiece $38, $10, 4, 1, 5, 0, 0, 2, 1
+ spritePiece $58, $10, 1, 1, 9, 0, 0, 2, 1
+ spritePiece $30, $18, 4, 2, $A, 0, 0, 2, 1
+ spritePiece $50, $18, 1, 2, $3C, 0, 0, 2, 1
+Map_b02d_1_End
+
 Map_obj5E_002A:	spriteHeader
-	spritePiece	$38, $10, 4, 1, 5, 0, 0, 2, 1
-	spritePiece	$58, $10, 1, 1, 9, 0, 0, 2, 1
-	spritePiece	$30, $18, 4, 2, $A, 0, 0, 2, 1
-	spritePiece	$50, $18, 1, 2, $3C, 0, 0, 2, 1
+ spritePiece $38, $10, 4, 1, $5AA, 0, 0, 2, 1
+ spritePiece $58, $10, 1, 1, $5AE, 0, 0, 2, 1
+ spritePiece $30, $18, 4, 2, $A, 0, 0, 2, 1
+ spritePiece $50, $18, 1, 2, $3C, 0, 0, 2, 1
 Map_obj5E_002A_End
 
 Map_obj5E_004C:	spriteHeader
-	spritePiece	$38, $10, 4, 1, $5AA, 0, 0, 2, 1
-	spritePiece	$58, $10, 1, 1, $5AE, 0, 0, 2, 1
-	spritePiece	$30, $18, 4, 2, $A, 0, 0, 2, 1
-	spritePiece	$50, $18, 1, 2, $3C, 0, 0, 2, 1
+ spritePiece -$14, $10, 4, 4, $26, 0, 0, 2, 1
+ spritePiece $C, $10, 1, 4, $36, 0, 0, 2, 1
 Map_obj5E_004C_End
 
-Map_obj5E_006E:	spriteHeader
-	spritePiece	-$14, $10, 4, 4, $26, 0, 0, 2, 1
-	spritePiece	$C, $10, 1, 4, $36, 0, 0, 2, 1
-Map_obj5E_006E_End
+Map_b02d_4:	spriteHeader
+ spritePiece -$68, $18, 4, 2, $A, 0, 0, 2, 1
+ spritePiece -$48, $18, 1, 2, $3C, 0, 0, 2, 1
+ spritePiece -$60, $11, 4, 1, $5DA, 0, 0, 1, 0
+ spritePiece -$40, $11, 1, 1, $5DE, 0, 0, 1, 0
+Map_b02d_4_End
 
 	even
