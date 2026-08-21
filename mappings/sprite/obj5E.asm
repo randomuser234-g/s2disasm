1b516fa50c69a2b33933365207d9da4586044729
randomuser234-g <thoratkihome@gmail.com>
2026-08-21 02:55:31 -0500

    accidentally set wrong priority on knux text
    caused issue where special stage could cover up K-T-E text

--- mappings/sprite/obj5E.asm
+++ mappings/sprite/obj5E.asm
@@ -1,4 +1,4 @@
-Map_9815: mappingsTable
+Map_cbfc: mappingsTable
 	mappingsTableEntry.w	Map_obj5E_0008
 	mappingsTableEntry.w	Map_b02d_1
 	mappingsTableEntry.w	Map_obj5E_002A
@@ -34,8 +34,8 @@ Map_obj5E_004C_End
 Map_b02d_4:	spriteHeader
  spritePiece -$68, $18, 4, 2, $A, 0, 0, 2, 1
  spritePiece -$48, $18, 1, 2, $3C, 0, 0, 2, 1
- spritePiece -$60, $11, 4, 1, $5DA, 0, 0, 1, 0
- spritePiece -$40, $11, 1, 1, $5DE, 0, 0, 1, 0
+ spritePiece -$60, $11, 4, 1, $5DA, 0, 0, 1, 1
+ spritePiece -$40, $11, 1, 1, $5DE, 0, 0, 1, 1
 Map_b02d_4_End
 
 	even
